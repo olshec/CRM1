c7de63cb7c006b7bd440ac28239cc686c75e9efd
olshec <olshec@gmail.com>
2019-08-25 02:20:43 -0400

    add CRUD for city

---
+++ models/Country.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Country".
+ *
+ * @property int $idCountry
+ * @property string $name
+ * @property int $countPeople
+ *
+ * @property City[] $cities
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Country';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'countPeople'], 'required'],
+            [['countPeople'], 'integer'],
+            [['name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idCountry' => 'Id Country',
+            'name' => 'Name',
+            'countPeople' => 'Count People',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCities()
+    {
+        return $this->hasMany(City::className(), ['Country_idCountry' => 'idCountry']);
+    }
+
+
+    
+}
---
+++ views/city/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\City */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="city-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'countPeople')->textInput() ?>
+
+    <?= $form->field($model, 'square')->textInput() ?>
+
+    <?= $form->field($model, 'Country_idCountry')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
