41753e044d109361039564c90f29cba2d8965af8
olshec <olshec@gmail.com>
2019-08-26 04:37:22 -0400

    add email

--- views/site/say.php
+++ views/site/say.php
@@ -1,4 +1,15 @@
 <?php
 use yii\helpers\Html;
 ?>
-<?= Html::encode($message) ?>
+<?= Html::encode($message) ;
+
+//var_dump(Yii::$app->components);
+
+Yii::$app->mailer->compose() // a view rendering result becomes the message body here
+    ->setFrom('user1@3520025571.com')
+    ->setTo('olshec@gmail.com')
+    ->setSubject('Message subject')
+    ->send();
+
+
+?>
