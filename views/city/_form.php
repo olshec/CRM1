ddfba82163d66a65f89406373edd16beb10cb73a
olshec <olshec@gmail.com>
2020-01-11 04:32:06 -0500

    few fixed

--- views/city/_form.php
+++ views/city/_form.php
@@ -25,7 +25,7 @@ use yii\widgets\ActiveForm;
         ->label('Страна') ?>
 
     <div class="form-group">
-        <?= Html::submitButton('SСохранить', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
